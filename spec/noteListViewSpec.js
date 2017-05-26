b7b4b981e229d13473b712ad27843a1f35bd97f7
david gray <davidseangray@gmail.com>
2017-05-26 15:06:15 +0100

    starting to test div ids have a value

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -1,14 +1,3 @@
-// (function() {
-//   var list = new listOfNotes();
-//   var note = new Note('testnote');
-//   var view = new noteListView();
-//   var controller = new noteController(list);
-//
-//   assert.isDefined(controller.list);
-//   console.log('controller is instantiated with a List model');
-//
-// })();
-
 (function() {
   var list = new listOfNotes();
   var view = new noteListView(list);
@@ -25,4 +14,13 @@
   list.writeNote("Bikes: Decent road ones that aren't built by Halfords, with poor groupsets and frame design");
   assert.isEqual(view.displayPage(), response);
   console.log("limits view content to 20 chars");
+})();
+
+(function() {
+  var list = new listOfNotes();
+  var view = new noteListView(list);
+  var response = "<ul><li><div id='0' >Bikes: Decent road o</div></li><li><div id='1' >Tony, went to the pa</div></li></ul>";
+  list.writeNote("Bikes: Decent road ones that aren't built by Halfords, with poor groupsets and frame design");
+  list.writeNote("Tony, went to the park and bought ice cream, it was not good ice cream... Dragonforce.");
+  assert.isEqual(view.displayPage(), response );
 })();
