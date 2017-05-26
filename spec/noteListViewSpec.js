f8d2747ef1d946c0c1ed20cd0eac4b105e18bb1e
Chris Mitchell <chrisjmit@gmail.com>
2017-05-26 13:22:54 +0100

    text length for view reduced to 20 chars

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -14,6 +14,15 @@
   var view = new noteListView(list);
   list.writeNote("Favourite food: pesto");
   list.writeNote("Favourite drink: seltzer");
-  assert.isEqual(view.displayPage(), "<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>");
+  assert.isEqual(view.displayPage(), "<ul><li><div>Favourite food: pest</div></li><li><div>Favourite drink: sel</div></li></ul>");
   console.log("view outputs html to be rendered on the page.");
+})();
+
+(function(){
+  var list = new listOfNotes();
+  var view = new noteListView(list);
+  var response = "<ul><li><div>Bikes: Decent road o</div></li></ul>";
+  list.writeNote("Bikes: Decent road ones that aren't built by Halfords, with poor groupsets and frame design");
+  assert.isEqual(view.displayPage(), response);
+  console.log("limits view content to 20 chars");
 })();
